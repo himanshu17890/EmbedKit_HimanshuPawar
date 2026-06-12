325e50b06d0d7dd8e6803f9267a646dafe0e0b14
Himanshu Pawar <himanshupawar259@gmail.com>
2026-06-12 18:06:08 +0530

    Added initialization function

--- ringbuf.c
+++ ringbuf.c
@@ -8,10 +8,24 @@ typedef struct{
     uint8_t head;
     uint8_t tail;
     uint8_t count;
-
-
 }RingBuffer;
+
+void ringbuf_init(RingBuffer *rb){
+    rb -> head = 0;
+    rb -> tail = 0;
+    rb -> count = 0;
+
+}
+
 int main (){
-    printf("Ring Buffer Assignment\n");
+    //printf("Ring Buffer Assignment\n");
+    RingBuffer rb;
+
+    ringbuf_init(&rb);
+
+    printf("Head = %d\n", rb.head);
+    printf("tail = %d\n", rb.tail);
+    printf("count = %d\n", rb.count);
+
     return 0;
 } 
